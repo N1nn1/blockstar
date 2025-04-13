8a87c2f39bdfd06fb87c0f65939045d21dfbd593
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2025-04-13 23:41:58 +0200

    First Commit

---
+++ src/main/java/com/ninni/minestrel/Minestrel.java
@@ -0,0 +1,14 @@
+package com.ninni.minestrel;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Minestrel.MODID)
+public class Minestrel {
+    public static final String MODID = "minestrel";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Minestrel() {
+    }
+}
