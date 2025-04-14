83f0f7a01dabcea0fce200738b1081ae9e0829f1
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2025-04-14 05:38:20 +0200

    Keyboard 4

--- src/main/java/com/ninni/minestrel/server/block/KeyboardBlock.java
+++ src/main/java/com/ninni/minestrel/server/block/KeyboardBlock.java
@@ -17,24 +17,28 @@ import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.*;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class KeyboardBlock extends BaseEntityBlock implements SimpleWaterloggedBlock {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    protected static final VoxelShape SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0);
+    public static final BooleanProperty BOTTOM = BooleanProperty.create("bottom");
+    public static final EnumProperty<KeyboardType> TYPE = EnumProperty.create("type", KeyboardType.class);
+    protected static final VoxelShape BOTTOM_SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0);
+    protected static final VoxelShape TOP_SHAPE = Block.box(0.0D, 8.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape STAND_SHAPE = Block.box(3, 0, 3, 13, 8, 13);
+    public static final VoxelShape TOP_STAND_SHAPE = Shapes.or(TOP_SHAPE, STAND_SHAPE);
 
     public KeyboardBlock() {
         super(Properties.of().strength(2.0F, 3.0F).sound(SoundType.WOOD).ignitedByLava());
-        this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false).setValue(FACING, Direction.NORTH));
+        this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false).setValue(BOTTOM, true).setValue(TYPE, KeyboardType.SINGLE).setValue(FACING, Direction.NORTH));
     }
 
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
@@ -74,23 +78,52 @@ public class KeyboardBlock extends BaseEntityBlock implements SimpleWaterloggedB
         return p_56397_.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(p_56397_);
     }
 
-    public BlockState updateShape(BlockState p_56381_, Direction p_56382_, BlockState p_56383_, LevelAccessor p_56384_, BlockPos p_56385_, BlockPos p_56386_) {
-        if (p_56381_.getValue(WATERLOGGED)) {
-            p_56384_.scheduleTick(p_56385_, Fluids.WATER, Fluids.WATER.getTickDelay(p_56384_));
+    public BlockState updateShape(BlockState state, Direction direction, BlockState state1, LevelAccessor levelAccessor, BlockPos pos, BlockPos pos1) {
+        if (state.getValue(WATERLOGGED)) {
+            levelAccessor.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(levelAccessor));
         }
-        return super.updateShape(p_56381_, p_56382_, p_56383_, p_56384_, p_56385_, p_56386_);
+        if (state != getConnectedShape(levelAccessor, state, pos)) levelAccessor.setBlock(pos, getConnectedShape(levelAccessor, state, pos), 3);
+
+        return super.updateShape(state, direction, state1, levelAccessor, pos, pos1);
     }
 
-    public VoxelShape getShape(BlockState p_56390_, BlockGetter p_56391_, BlockPos p_56392_, CollisionContext p_56393_) {
-        return SHAPE;
+    public BlockState getConnectedShape(LevelAccessor levelAccessor, BlockState state, BlockPos pos) {
+        BlockState rightState = levelAccessor.getBlockState(pos.relative(state.getValue(FACING).getCounterClockWise(), 1));
+        BlockState leftState = levelAccessor.getBlockState(pos.relative(state.getValue(FACING).getClockWise(), 1));
+
+        if (leftState.getBlock() instanceof KeyboardBlock || rightState.getBlock() instanceof KeyboardBlock) {
+
+            boolean isLeftBottom = leftState.getBlock() instanceof KeyboardBlock && leftState.getValue(BOTTOM) == state.getValue(BOTTOM) && leftState.getValue(FACING) == state.getValue(FACING);
+            boolean isRightBottom = rightState.getBlock() instanceof KeyboardBlock && rightState.getValue(BOTTOM) == state.getValue(BOTTOM) && rightState.getValue(FACING) == state.getValue(FACING);
+
+            if (!isRightBottom && isLeftBottom) {
+                return state.setValue(TYPE, KeyboardType.RIGHT);
+            }
+            else if (!isLeftBottom && isRightBottom) {
+                return state.setValue(TYPE, KeyboardType.LEFT);
+            }
+            else if (isLeftBottom && isRightBottom) return state.setValue(TYPE, KeyboardType.MIDDLE);
+        }
+        return state.setValue(TYPE, KeyboardType.SINGLE);
+    }
+
+    public VoxelShape getShape(BlockState state, BlockGetter p_56391_, BlockPos p_56392_, CollisionContext p_56393_) {
+        return state.getValue(BOTTOM) ? BOTTOM_SHAPE : state.getValue(TYPE) != KeyboardType.MIDDLE ? TOP_STAND_SHAPE : TOP_SHAPE;
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_56388_) {
-        p_56388_.add(WATERLOGGED, FACING);
+        p_56388_.add(WATERLOGGED, FACING, BOTTOM, TYPE);
     }
 
-    public BlockState getStateForPlacement(BlockPlaceContext p_54481_) {
-        return this.defaultBlockState().setValue(FACING, p_54481_.getHorizontalDirection().getOpposite());
+
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        BlockPos blockpos = ctx.getClickedPos();
+
+        FluidState fluidstate = ctx.getLevel().getFluidState(blockpos);
+        Direction direction = ctx.getClickedFace();
+        BlockState blockstate1 = this.defaultBlockState().setValue(BOTTOM, true).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER).setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+        return direction != Direction.DOWN && (direction == Direction.UP || !(ctx.getClickLocation().y - (double)blockpos.getY() > 0.5D)) ? blockstate1 : blockstate1.setValue(BOTTOM, false);
     }
 
     public BlockState rotate(BlockState p_54540_, Rotation p_54541_) {
