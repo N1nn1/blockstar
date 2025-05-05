1e80b36411c8d3ccc9f3cbab57c489d1828228ea
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2025-05-05 20:10:15 +0200

    Fixed soundfont category thingie

--- src/main/java/com/ninni/blockstar/client/misc/text/ResonantPrismTooltip.java
+++ src/main/java/com/ninni/blockstar/client/misc/text/ResonantPrismTooltip.java
@@ -33,7 +33,7 @@ public class ResonantPrismTooltip implements ClientTooltipComponent {
 
         if (icon.getPath().contains("-")) {
             resourceLocation = new ResourceLocation(icon.getNamespace(), "textures/soundfont/" + icon.getPath().split("-")[1] + ".png");
-            ResourceLocation noteBlockResourceLocation = new ResourceLocation(Blockstar.MODID, "textures/soundfont/category/" + icon.getPath().split("-")[0] + ".png");
+            ResourceLocation noteBlockResourceLocation = new ResourceLocation(icon.getNamespace(),"textures/soundfont/category/" + icon.getPath().split("-")[0] + ".png");
             graphics.blit(noteBlockResourceLocation, x, y - 2, 0, 0, this.getWidth(font), this.getHeight(), this.getWidth(font), this.getHeight());
             x += 24;
         }
