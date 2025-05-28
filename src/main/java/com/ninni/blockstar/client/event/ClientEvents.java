34b22fac981a15f2550ba743d5d03e3c799558ad
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2025-05-28 04:48:21 +0200

    Items in the creative inventory

--- src/main/java/com/ninni/blockstar/client/event/ClientEvents.java
+++ src/main/java/com/ninni/blockstar/client/event/ClientEvents.java
@@ -7,7 +7,9 @@ import com.ninni.blockstar.server.data.SoundfontManager;
 import com.ninni.blockstar.server.item.ResonantPrismItem;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.util.MutableHashedLinkedMap;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -54,6 +56,26 @@ public class ClientEvents {
             for (SoundfontManager.SoundfontDefinition data : categorized) {
                 entries.put(ResonantPrismItem.getPrismItemFromSoundfont(data), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
             }
+        }
+
+
+        if (key == CreativeModeTabs.BUILDING_BLOCKS) {
+            entries.putAfter(Items.AMETHYST_BLOCK.getDefaultInstance(), BItemRegistry.RESONANT_AMETHYST_BLOCK.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
+        if (key == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
+            entries.putAfter(Items.JUKEBOX.getDefaultInstance(), BItemRegistry.METRONOME.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            entries.putAfter(BItemRegistry.METRONOME.get().getDefaultInstance(), BItemRegistry.KEYBOARD.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            entries.putAfter(Items.LOOM.getDefaultInstance(), BItemRegistry.COMPOSING_TABLE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
+        if (key == CreativeModeTabs.REDSTONE_BLOCKS) {
+            entries.putAfter(Items.JUKEBOX.getDefaultInstance(), BItemRegistry.METRONOME.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
+        if (key == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            entries.putBefore(Items.MUSIC_DISC_13.getDefaultInstance(), BItemRegistry.MUSIC_DISC_TEMPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
+        if (key == CreativeModeTabs.INGREDIENTS) {
+            entries.putAfter(Items.AMETHYST_SHARD.getDefaultInstance(), BItemRegistry.RESONANT_AMETHYST_SHARD.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            entries.putAfter(BItemRegistry.RESONANT_AMETHYST_SHARD.get().getDefaultInstance(), BItemRegistry.RESONANT_PRISM.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         }
     }
 
